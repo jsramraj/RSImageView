aec779c70de0cf16890c38a5beb10b5df12ff0d2
Ramaraj T <ramarajthanga.p@zanec.com>
2014-11-25 12:51:53 +0530

    New project files are added to the Git.

---
+++ RSImageView/RSImageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSImageView
+//
+//  Created by Ramaraj Thanga Pandi on 25/11/14.
+//  Copyright (c) 2014 Ramaraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RSImageView/RSImageView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RSImageView
+//
+//  Created by Ramaraj Thanga Pandi on 25/11/14.
+//  Copyright (c) 2014 Ramaraj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
